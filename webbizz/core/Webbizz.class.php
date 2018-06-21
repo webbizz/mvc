658372ab0968ccfbe0db2d763af7d34b6f45ef5a
Scott <webbizz65@gmail.com>
2018-06-21 02:54:22 -0700

    Created application initializer

---
+++ webbizz/core/Webbizz.class.php
@@ -0,0 +1,16 @@
+/**
+ * This is the file that initializes the session
+ * creates our global variables/paths etc.
+ */
+ class Webbizz  {
+ 
+    private static function run()  {
+    
+        self::init();
+        
+    }
+    
+    public function init()  {
+    
+    }
+ }
